5d4e85213ffc05a0c01ecd222254ae10802c5684
rudevelooper <rudevelooper@gmail.com>
2019-03-09 10:08:24 +0600

    0001

---
+++ devcontroller/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DevcontrollerConfig(AppConfig):
+    name = 'devcontroller'
---
+++ device_list/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DeviceListConfig(AppConfig):
+    name = 'device_list'
